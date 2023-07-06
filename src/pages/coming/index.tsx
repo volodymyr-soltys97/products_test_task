5fa5b181832300cb1ac53dc0862b99b31730259b
Volodymyr Soltys <soltus.vovan@gmail.com>
2023-07-06 17:41:14 +0300

    Add content to coming page

--- src/pages/coming/index.tsx
+++ src/pages/coming/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+/* eslint-disable quote-props */
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
 import classes from '../../styles/pages.module.scss';
+import ordersData from '../../data/orders.json';
+import ComingItem from '../../components/ComingItem';
 
 const Coming = () => {
+  const [orders, setOrders] = useState([ordersData]);
+
+  // console.log(ordersData);
+  // console.log(orders);
+
+  useEffect(() => setOrders(() => [...orders]), []);
+
   return (
     <div className={classes.container}>
       Coming Page
+      {orders && orders.map(order => order.map(item => (
+        <ComingItem key={item.id} orders={item} />
+      )))}
     </div>
   );
 };
---
+++ src/types/orders.ts
@@ -0,0 +1,32 @@
+export interface Price {
+  isDefault: number;
+  symbol: string;
+  value: number;
+}
+
+export interface Guarantee {
+  start: string;
+  end: string;
+}
+
+export interface Products {
+  date: string;
+  guarantee: Guarantee;
+  id: number;
+  isNew: number;
+  order: 1;
+  photo: string;
+  price: Price[];
+  serialNumber: number;
+  specification: string;
+  title: string;
+  type: string;
+}
+
+export interface Order {
+  date: string;
+  description: string;
+  id: number;
+  products: Products[];
+  title: string;
+}
