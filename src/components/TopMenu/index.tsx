3a90a7e478f66d4b7ad7ada743790e270670e358
Volodymyr Soltys <soltus.vovan@gmail.com>
2023-07-06 18:20:24 +0300

    fix logo link

--- src/components/TopMenu/index.tsx
+++ src/components/TopMenu/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useState } from 'react';
 // import Image from 'next/image';
+import { Link } from 'react-router-dom';
 import logo from '../../images/logo.jpg';
 import classes from './styles.module.scss';
 import { formatDate, formatTime } from '../../helpers/functions';
@@ -18,9 +19,9 @@ const TopMenu: React.FC = () => {
     <header className={classes.header}>
       <div className={classes.header__container}>
         <div className={classes.logo_search}>
-          <a href="#">
+          <Link to="/">
             <img className={classes.logo} src={logo} alt="" />
-          </a>
+          </Link>
           <input className={classes.search} type="search" placeholder="Search" />
         </div>
         <div className={classes.date__container}>
